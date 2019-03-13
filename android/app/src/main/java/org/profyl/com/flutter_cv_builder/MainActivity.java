0d6bfcdbf7785bf23531f848a5e1e1ef33501228
bipin <jazzbpn@gmail.com>
2019-03-13 10:48:30 +0545

    Project Init

---
+++ android/app/src/main/java/org/profyl/com/flutter_cv_builder/MainActivity.java
@@ -0,0 +1,13 @@
+package org.profyl.com.flutter_cv_builder;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  final appTitle = 'Drawer Demo';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: appTitle,
+      home: MyHomePage(title: appTitle),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final String title;
+
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Nepali",
+            style: TextStyle(color: Colors.white),
+            textDirection: TextDirection.ltr),
+        backgroundColor: Colors.brown,
+      ),
+      body: SnackBarPage(),
+      drawer: Drawer(
+        // Add a ListView to the drawer. This ensures the user can scroll
+        // through the options in the Drawer if there isn't enough vertical
+        // space to fit everything.
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child:
+                  Text('Drawer Header', style: TextStyle(color: Colors.white)),
+              decoration: BoxDecoration(
+                color: Colors.brown,
+              ),
+            ),
+            ListTile(
+              title: Text('Navigate Screen'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              subtitle: Text("Some subtitle"),
+              title: Text('Overlap Screen'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SnackBarPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: RaisedButton(
+        onPressed: () {
+          final snackBar = SnackBar(
+            content: Text('Yay! A SnackBar!'),
+            action: SnackBarAction(
+              label: 'Undo',
+              onPressed: () {
+                // Some code to undo the change!
+              },
+            ),
+          );
+
+          // Find the Scaffold in the Widget tree and use it to show a SnackBar!
+          Scaffold.of(context).showSnackBar(snackBar);
+        },
+        child: Text('Show SnackBar'),
+      ),
+    );
+  }
+}
